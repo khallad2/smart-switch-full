1eafbe1318444d7bd9b14354d15c4a8f07b91a37
khallad2 <khallad_3@hotmail.com>
2023-08-17 18:28:38 +0200

    feat: update switch and implement card UI

--- Mobile/SmartSwitch/switch/switch.js
+++ Mobile/SmartSwitch/switch/switch.js
@@ -1,35 +1,62 @@
-// Switch.js
 import React, { useState } from 'react';
-import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+import { TouchableOpacity, Text, StyleSheet, View } from 'react-native';
+import { Card, TextInput, Button } from 'react-native-paper';
 
 const Switch = ({ onPress, switched }) => {
+    const [deviceAddress, setDeviceAddress] = useState('');
+    const [wifiName, setWifiName] = useState('');
+    const [wifiPassword, setWifiPassword] = useState('');
+
+    const fetchData = async () => {
+        try {
+            const END_POINT = `http://${deviceAddress}/setAngle?switch=1`; // Build the END_POINT string with user-entered IP
+            const response = await fetch(END_POINT);
+            setSwitched((prevSwitched) => !prevSwitched);
+            return response;
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
     return (
-        <TouchableOpacity
-            style={[styles.button, switched && styles.buttonActive]}
-            onPress={onPress}
-            activeOpacity={0.7}
-        >
-            <Text style={styles.buttonText}>
-                {switched ? 'Switched ON' : 'Switched OFF'}
-            </Text>
-        </TouchableOpacity>
+        <View>
+            <Card style={styles.card}>
+                <Card.Content>
+                    <TextInput
+                        label="Device Address"
+                        value={deviceAddress}
+                        onChangeText={setDeviceAddress}
+                    />
+                    <TextInput
+                        label="WiFi Name"
+                        value={wifiName}
+                        onChangeText={setWifiName}
+                    />
+                    <TextInput
+                        label="WiFi Password"
+                        value={wifiPassword}
+                        onChangeText={setWifiPassword}
+                        secureTextEntry
+                    />
+                </Card.Content>
+                <Card.Actions style={styles.cardActions}>
+                    <Button onPress={fetchData} mode="contained">
+                        {switched ? 'Switched ON' : 'Switched OFF'}
+                    </Button>
+                </Card.Actions>
+            </Card>
+        </View>
     );
 };
 
 const styles = StyleSheet.create({
-    button: {
-        backgroundColor: '#FFA500',
-        borderRadius: 50,
-        paddingVertical: 15,
-        paddingHorizontal: 30,
+    card: {
         marginVertical: 10,
+        marginHorizontal: 20,
+        padding: 10,
     },
-    buttonActive: {
-        backgroundColor: '#4CAF50',
-    },
-    buttonText: {
-        color: '#ffffff',
-        fontSize: 18,
+    cardActions: {
+        justifyContent: 'center',
     },
 });
 
